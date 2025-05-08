bcace5331961b912069d3ea3c4b003c0a4d4e994
mdsulemannadeem <mdsulemannadeem.1234@gmail.com>
2025-05-08 22:55:46 +0530

    hell2

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dbgr = require('debug')('app:mongoose');
+const config = require('config');
 
-// Use environment variable for MongoDB URI with fallback to local
-const mongoURI = `${require('config').get('MONGODB_URI')}`;
-
-mongoose.connect(mongoURI)
+mongoose.connect(`${config.get('MONGODB_URI')}`)
     .then(() => dbgr('MongoDB connected successfully!'))
     .catch(err => dbgr('MongoDB connection error:', err));
+
+
 
 module.exports = mongoose.connection;
